b088fd26c345779a292182ad9247fbb49d0c3fb8
Дмитрий Домбаян <dmitriy@MacBook-Air-Dmitrij.local>
2023-03-06 10:58:24 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),  WindiCSS(),],
-  base: 'https://worldlyth.github.io/'
+  base: 'https://worldlyth.github.io/spacex/'
 })
